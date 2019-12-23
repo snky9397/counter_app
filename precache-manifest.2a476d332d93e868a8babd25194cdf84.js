c59118ce4310a2265f42307d17ae07b0aac59f01
sbhatia9397 <sankalp.bhatia@agiliad.com>
2019-12-23 18:06:52 +0530

    Updates

---
+++ precache-manifest.2a476d332d93e868a8babd25194cdf84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ec0d2ac1492f2f3927c8c6349dcbceb",
+    "url": "/counter_app/index.html"
+  },
+  {
+    "revision": "6ed4e83478549662589c",
+    "url": "/counter_app/static/css/2.f65acde2.chunk.css"
+  },
+  {
+    "revision": "f0f96797ad85b30968f7",
+    "url": "/counter_app/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "6ed4e83478549662589c",
+    "url": "/counter_app/static/js/2.ed798d31.chunk.js"
+  },
+  {
+    "revision": "f0f96797ad85b30968f7",
+    "url": "/counter_app/static/js/main.1b0f60de.chunk.js"
+  },
+  {
+    "revision": "70da332cac7fb580def1",
+    "url": "/counter_app/static/js/runtime-main.9faab710.js"
+  }
+]);
